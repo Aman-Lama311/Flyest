e81bd50639fd708116cbc40ab13c337b29255ada
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-05-20 17:55:36 +0545

    added new favicon

---
+++ src/app/itinerary-black/reusableNewCopms/RouteOverview.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const RouteOverview = () => {
+  return (
+    <div id="route-overview" className="mt-10">
+      <h1 className="text-3xl font-semibold ">Route Overview</h1>
+      <p className="mt-4 text-gray-700">
+        Experience a spectacular journey as you fly over Nepal’s diverse landscapes, from lush valleys to snow-capped peaks. Our heli-tours take you above remote mountain villages, offering unique aerial views of the majestic Himalayas without the challenge of trekking. <br />
+        <br />
+        Witness panoramic vistas of iconic peaks such as Manaslu and Annapurna, all from the comfort of a helicopter. This breathtaking route combines convenience and adventure, allowing you to explore the grandeur of Nepal’s mountains in style. Whether it’s a day trip or part of a larger itinerary, the views from above provide unforgettable memories and a fresh perspective on the world’s highest peaks.
+      </p>
+    </div>
+  );
+};
+
+export default RouteOverview;
