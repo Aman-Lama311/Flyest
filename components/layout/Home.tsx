09555d7670e279648da45071e387ea674e2ddb90
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-05-20 20:28:59 +0545

    fixed deployment error

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Hero from '../HeroComponents/Hero'
-import About from '../about/page.tsx'
+import About from '../about/page'
 import Cards from '../cards/Cards'
 import WhatWeDo from '../whatwedo/WhatWeDo'
 import Blog from '../blog/Blog'
